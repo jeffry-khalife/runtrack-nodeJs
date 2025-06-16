09c0bff9adfa905a19c3f690093b3a78147bef7a
jeffry-khalife <jeffry.khalife@laplateforme.io>
2025-06-16 15:03:21 +0200

    Job 5 of day 1 finished

---
+++ Jour1/job05/index.js
@@ -0,0 +1,5 @@
+const path = require('node:path');
+const index = 'C:/Users/jblun/Desktop/runtrack-nodeJs/Jour1/job05/index.js';
+console.log(path.dirname(index)); 
+console.log(path.basename(index));
+console.log(path.extname(index)); 
